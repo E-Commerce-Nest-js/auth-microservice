9c579a2f85a6f7e9e55dad5c97c774bf613eeb52
Igor Tkachenko <stiles.2126@gmail.com>
2022-01-22 18:51:35 +1000

    feat: global prefix 'api'

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
     const app = await NestFactory.create(AppModule);
+    app.use(cookieParser());
+    app.setGlobalPrefix('api');
     await app.listen(3000);
 }
 bootstrap();
