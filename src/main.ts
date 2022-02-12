0efe4293539c27412f71308242cb01bbc0c80c39
Igor Tkachenko <stiles.2126@gmail.com>
2022-02-13 01:39:09 +1000

    feat: add global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap(): Promise<void> {
     const app = await NestFactory.create(AppModule);
     app.use(cookieParser());
+    app.useGlobalPipes(new ValidationPipe());
     app.setGlobalPrefix('api');
     await app.listen(3000);
 }
